13c61a5bb55177128716777bd228d9f7a167a883
Melanie Tai <mt337@cornell.edu>
2022-06-20 15:23:05 -0700

    Complete read n characters given read4

---
+++ array_and_strings/read_n_characters_given_read4.py
@@ -0,0 +1,82 @@
+"""
+Given a file and assume that you can only read the file using a given method read4, implement a method to read n characters.
+
+Method read4:
+
+The API read4 reads four consecutive characters from file, then writes those characters into the buffer array buf4.
+
+The return value is the number of actual characters read.
+
+Note that read4() has its own file pointer, much like FILE *fp in C.
+
+Definition of read4:
+
+    Parameter:  char[] buf4
+    Returns:    int
+
+buf4[] is a destination, not a source. The results from read4 will be copied to buf4[].
+Below is a high-level example of how read4 works:
+
+
+File file("abcde"); // File is "abcde", initially file pointer (fp) points to 'a'
+char[] buf4 = new char[4]; // Create buffer with enough space to store characters
+read4(buf4); // read4 returns 4. Now buf4 = "abcd", fp points to 'e'
+read4(buf4); // read4 returns 1. Now buf4 = "e", fp points to end of file
+read4(buf4); // read4 returns 0. Now buf4 = "", fp points to end of file
+ 
+
+Method read:
+
+By using the read4 method, implement the method read that reads n characters from file and store it in the buffer array buf. Consider that you cannot manipulate file directly.
+
+The return value is the number of actual characters read.
+
+Definition of read:
+
+    Parameters:	char[] buf, int n
+    Returns:	int
+
+buf[] is a destination, not a source. You will need to write the results to buf[].
+Note:
+
+Consider that you cannot manipulate the file directly. The file is only accessible for read4 but not for read.
+The read function will only be called once for each test case.
+You may assume the destination buffer array, buf, is guaranteed to have enough space for storing n characters.
+
+The read4 API is already defined for you.
+
+    @param buf4, a list of characters
+    @return an integer
+    def read4(buf4):
+
+# Below is an example of how the read4 API can be called.
+file = File("abcdefghijk") # File is "abcdefghijk", initially file pointer (fp) points to 'a'
+buf4 = [' '] * 4 # Create buffer with enough space to store characters
+read4(buf4) # read4 returns 4. Now buf = ['a','b','c','d'], fp points to 'e'
+read4(buf4) # read4 returns 4. Now buf = ['e','f','g','h'], fp points to 'i'
+read4(buf4) # read4 returns 3. Now buf = ['i','j','k',...], fp points to end of file
+"""
+
+class Solution:
+    def read(self, buf, n):
+        """
+        :type buf: Destination buffer (List[str])
+        :type n: Number of characters to read (int)
+        :rtype: The number of actual characters read (int)
+        """
+        # n = num of chars to be read and store to buf
+        # read4(buf4) => return num of chars read (max of 4) and store to buf4
+        # copy buf4 to buf continuously until n=0 or until buf4 returns none (break loop)
+        
+        idx = 0 # copy to buf start at index 0
+        while n:
+            buf4 = [""] * 4 #placeholder for buf4
+            count = read4(buf4)
+            if not count:
+                break
+            min_count = min(count, n) # only write min_count # of chars into buf
+            buf[idx:idx+min_count] = buf4[:min_count]
+            idx += min_count
+            n -= min_count
+        
+        return idx
