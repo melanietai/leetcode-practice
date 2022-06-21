b73bffe765ed872e403b9c7b4949e2cf056d9daf
Melanie Tai <mt337@cornell.edu>
2022-06-21 15:09:31 -0700

    Complete merge sorted array

---
+++ array_and_strings/merge_sorted_array.py
@@ -0,0 +1,30 @@
+"""
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+"""
+
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        # 3 pointers, one to m-1, one to n-1, one for looping backwards starting at end of nums1
+        # p2 cannot be less than 0 (out of range) --> break
+        # p1 cannot be less than 0 --> less than 0 will mean all other nums left in nums2 is less than smallest num in num1, so keep adding the remaning nums2 to front of array until p reaches index 0
+        
+        p1 = m - 1
+        p2 = n - 1
+        
+        for p in range(n + m - 1, -1, -1):
+            if p2 < 0:
+                break
+            if p1 >= 0 and nums1[p1] > nums2[p2]:
+                nums1[p] = nums1[p1]
+                p1 -= 1
+            else:
+                nums1[p] = nums2[p2]
+                p2 -= 1
