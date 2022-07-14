2aaf389a9dd781eeef88ccce5612a602abe2e5f5
Melanie Tai <mt337@cornell.edu>
2022-07-13 22:45:55 -0700

    Add delete_recursive method

--- trees_and_graphs/binary_search_tree.py
+++ trees_and_graphs/binary_search_tree.py
@@ -34,7 +34,54 @@ class BinaryTreeNode:
     
     def insert_iteration(self, root, val):
         node = root
+
+        while node:
+            if val < node.val:
+                if not node.left:
+                    node.left = BinaryTreeNode(val)
+                    return root
+                else:
+                    node = node.left
+            else:
+                if not node.right:
+                    node.right = BinaryTreeNode(val)
+                    return root
+                else:
+                    node = node.right
+
+        return BinaryTreeNode(val)
+
+    def delete_recursive(self, root, val):
+        def successor(self, root):
+            root = root.right
+            while root.left:
+                root = root.left
+            return root 
         
+        def predecessor(self, root):
+            root = root.left 
+            while root.right:
+                root = root.right
+            return root
+
+        if not root:
+            return root
         
+        if val < root.left:
+            root.left = self.delete_recursive(root.left, val)
+        elif val > root.right:
+            root.right = self.delete_recursive(root.right, val)
+        else:
+            if not (root.left or root.right):
+                root = None
+            elif root.right:
+                root.val = self.successor(root)
+                root.right = self.delete_recursive(root.right, root.val)
+            else:
+                root.val = self.predessor(root)
+                root.left = self.delete_recursive(root.left, root.val)
+        return root
+        
+
         
 
