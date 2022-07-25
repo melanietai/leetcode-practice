fcee30a708e4fe05e1079dfe845afc1c1e2ccee6
Melanie Tai <mt337@cornell.edu>
2022-07-24 21:59:47 -0700

    Modify code

--- linked_lists/add_two_nums.py
+++ linked_lists/add_two_nums.py
@@ -4,31 +4,40 @@
 #         self.val = val
 #         self.next = next
 class Solution:
+
+
+    def helper(self, l1, l2):
+        if not l1 and not l2:
+            return 
+        pos, sum = self.helper(l1.next, l2.next)
+        10 ^ pos * (l1 + l2)
+
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        dummy = ListNode()
-        d_cur = dummy
-        cur1 = l1
-        cur2 = l2
-        carry = 0
-        
-        while cur1 or cur2 or carry != 0:
-            if cur1:
-                x = cur1.val
-            else:
-                x = 0
-            if cur2:
-                y = cur2.val
-            else:
-                y = 0
+        # dummy = ListNode()
+        # d_cur = dummy
+        # cur1 = l1
+        # cur2 = l2
+        # carry = 0
+
+        # while cur1 or cur2 or carry != 0:
+        #     if cur1:
+        #         x = cur1.val
+        #     else:
+        #         x = 0
+        #     if cur2:
+        #         y = cur2.val
+        #     else:
+        #         y = 0
             
-            sum = x + y + carry
-            carry = sum // 10
-            d_cur.next = ListNode(sum % 10)
-            if cur1:
-                cur1 = cur1.next
-            if cur2:
-                cur2 = cur2.next
-            d_cur = d_cur.next
+        #     sum = x + y + carry
+        #     carry = sum // 10
+        #     d_cur.next = ListNode(sum % 10)
+        #     if cur1:
+        #         cur1 = cur1.next
+        #     if cur2:
+        #         cur2 = cur2.next
+        #     d_cur = d_cur.next
+        
         
         return dummy.next
         
