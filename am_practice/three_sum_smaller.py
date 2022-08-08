09f255e32ce3de8a5d424d0382ad7fe97429f388
Melanie Tai <mt337@cornell.edu>
2022-08-08 12:01:50 -0700

    Complete three sum closest

---
+++ am_practice/three_sum_smaller.py
@@ -0,0 +1,20 @@
+# two pointers solution 1
+class Solution:
+    def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        nums.sort()
+        total = 0
+        for i in range(len(nums)-2):
+            total += self.two_sum_smaller(nums, i+1, target-nums[i])
+        return total
+        
+        
+    def two_sum_smaller(self, nums, start_ind, target):
+        s, e = start_ind, len(nums)-1
+        total = 0
+        while s < e:
+            if nums[s] + nums[e] < target:
+                total += e - s
+                s += 1
+            else:
+                e -= 1
+        return total
